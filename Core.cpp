88ce7ba8334357bec2c56c16e438daa1fab09931
dudepop334 <beanpanther@gmail.com>
2025-09-01 16:35:44 -0600

    Create Core.cpp

---
+++ Core.cpp
@@ -0,0 +1,8 @@
+// sentiment.cpp
+#include <iostream>
+int main() {
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << "Neutral" << std::endl; // placeholder
+    return 0;
+}
